676524c1bfee3e311087919565f8ff5a1dd4ad4c
ange melissa <nguessanmelissa01@gmail.com>
2023-01-23 21:36:10 +0000

    creation de projet et apps django

---
+++ django-web-app/merchex/listings/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
+
+# Create your views here.
+
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
